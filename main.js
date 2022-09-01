65428414ed6463a0646afb7a3fc5fdd1e74743c5
Sabrina Horisberger <sabrinahorisberger@gmail.com>
2022-09-01 19:30:15 -0300

    ciclos

---
+++ main.js
@@ -0,0 +1,18 @@
+
+alert("Hola! bienvenidx a nuestra agenda nocturna")
+
+let nombreIngresado = prompt("Ingrese su nombre:");
+alert("que bueno tenerte por aquí, " + nombreIngresado)
+
+let edad = parseInt (prompt ( "Ingrese su edad: "));
+
+if((edad<18)){
+     
+    alert(nombreIngresado + " lamentablemente no cumples con los requisitos de ingreso, nos vemos en un par de años :)")
+     }
+    
+     while(isNaN(edad) || edad <=18){
+        alert("Bienvenido!");
+     }
+
+     alert ("Los Clubs nocturnos que participan de nuestra agenda son:")
