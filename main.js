9f98d1f6999f816db6926d42c851d06fc7811f45
Sabrina Horisberger <sabrinahorisberger@gmail.com>
2022-09-05 00:10:27 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
+alert("Hola! bienvenidx a nuestra agenda nocturna");
 
-alert('Hola! bienvenidx a nuestra agenda nocturna')
+let nombreIngresado = prompt("Ingrese su nombre:");
+alert("Que bueno tenerte por aquí, " + nombreIngresado);
 
-let nombreIngresado = prompt('Ingrese su nombre:');
-alert('que bueno tenerte por aquí, ' + nombreIngresado)
+let edad;
+do {
+  edad = parseInt(prompt("Ingrese su edad: "));
 
-let edad 
-do{
-    edad = parseInt (prompt ( 'Ingrese su edad: '));
+  if (edad < 18) {
+    alert(
+      nombreIngresado +
+        " lamentablemente no cumples con los requisitos de ingreso, nos vemos en un par de años :)"
+    );
+  }
+} while (isNaN(edad) || edad < 18);
 
-    if((edad<18)){
-     
-    alert(nombreIngresado +  'lamentablemente no cumples con los requisitos de ingreso, nos vemos en un par de años :)')
-     }
-    
-    
-} while(isNaN(edad) || edad <=18){
-        alert('Bienvenido!');
-     }
-    
+alert("Bienvenido!");
 
-     alert ('Los Clubs nocturnos que participan de nuestra agenda son:')
+alert("Los Clubs nocturnos que participan de nuestra agenda son:");
