191033fc4119088e006ccdad51d07b45efa97940
Sabrina Horisberger <sabrinahorisberger@gmail.com>
2022-09-29 22:35:24 -0300

    lclstorg

--- js/main.js
+++ js/main.js
@@ -1,55 +1,61 @@
- 
- const botonDarkMode = document.getElementById('botonDarkMode');
- const botonLightMode = document.getElementById('botonLightMode');
- 
- let darkMode
- 
- if(localStorage.getItem('theme')){
-   darkMode = localStorage.getItem('theme');
+const checkearEdad = () =>{
+  return edad >= 18 || edad === undefined
+}
 
- }else{
+
+let edad;
+
+const botonDarkMode = document.getElementById('botonDarkMode');
+const botonLightMode = document.getElementById('botonLightMode');
+
+let darkMode
+
+if(localStorage.getItem('theme')){
+  darkMode = localStorage.getItem('theme');
+}else{
     localStorage.setItem('theme', "light");
- }
- 
- if(darkMode == "dark"){
+}
+
+if(darkMode == "dark"){
   document.body.classList.add('darkMode');
- }
+}
 
- botonDarkMode.addEventListener('click', () =>{
-   document.body.classList.add('darkMode');
-   localStorage.setItem('theme', "dark");
+botonDarkMode.addEventListener('click', () =>{
+  document.body.classList.add('darkMode');
+  localStorage.setItem('theme', "dark");
+});
 
-
- });
-
- botonLightMode.addEventListener('click', () =>{
+botonLightMode.addEventListener('click', () =>{
     document.body.classList.remove('darkMode');
     localStorage.setItem('theme', "light");
+});
 
 
-
- });
-
-
-const devolverNombre = () => {
   const input1 = document.getElementById("preguntarNombre2");
 
   input1.addEventListener("change", (e) => {
-    let nombre = e.target.value;
-    let titulo = document.getElementById("saludo2");
-    if (nombre.lenght !== 0) {
-      titulo.innerText = "Que bueno tenerte por aquí, " + nombre;
-    }
-  });
-};
+    if(checkearEdad()){
+      let nombre = e.target.value;
+      let titulo = document.getElementById("saludo2");
+      if (nombre.lenght !== 0) {
+        titulo.innerText = "Que bueno tenerte por aquí, " + nombre;
+      }
+    }else{
+    alert("No tienes edad suficiente")
+  }
+});
+
+
 
 let listaBoliches = document.getElementById("listaBoliches");
-let edad;
+
+
 
 const comprobarEdad = () => {
-  const input2 = document.getElementById("edades");
-  input2.addEventListener("change", (e) => {
-    let edad = e.target.value;
+
+    const input2 = document.getElementById("edades");
+    input2.addEventListener("change", (e) => {
+    edad = e.target.value;
     console.log(edad);
     if (edad < 18) {
       listaBoliches.innerHTML =
@@ -60,24 +66,28 @@ const comprobarEdad = () => {
     }
   });
 };
+comprobarEdad();
 
 const sumar = (gastoEntrada, gastoParking) => {
   return Number(gastoEntrada) + Number(gastoParking);
 };
 
 const cuantoGastar = () => {
-  let debajoDosmil = document.getElementById("debajoDosmil");
-  const input3 = document.getElementById("gastoEntrada").value;
-  const input4 = document.getElementById("gastoParking").value;
-  let total = sumar(input3, input4);
-  console.log(total);
-
-  if (total < 2000) {
-    debajoDosmil.innerHTML =
-      ' <p> "Los lugares para salir por debajo de los 2000 pesos son: La Luciernaga, Baby Baby, El Faro, Inmortal." <p>';
-  } else if (total >= 2000) {
-    debajoDosmil.innerHTML =
-      "Por ese rango de precio podes salir a todos esos lugares: Diosa, El Agujero, Sunshine, Black Velvet, Red Point";
+  if(checkearEdad()){
+    let debajoDosmil = document.getElementById("debajoDosmil");
+    const input3 = document.getElementById("gastoEntrada").value;
+    const input4 = document.getElementById("gastoParking").value;
+    let total = sumar(input3, input4);
+    console.log(total);
+    if (total < 2000) {
+      debajoDosmil.innerHTML =
+        ' <p> "Los lugares para salir por debajo de los 2000 pesos son: La Luciernaga, Baby Baby, El Faro, Inmortal." <p>';
+    } else if (total >= 2000) {
+      debajoDosmil.innerHTML =
+        "Por ese rango de precio podes salir a todos esos lugares: Diosa, El Agujero, Sunshine, Black Velvet, Red Point";
+    }
+  }else{
+    alert("No tienes edad suficiente")
   }
 };
 
@@ -90,44 +100,41 @@ const busquedaNombres = function (strTipo) {
   return nombre.join(" ");
 };
 
-const tipoDeExperiencia = () => {
-  let askAgain = true;
-  const input5 = document.getElementById("tipoExperiencia");
-  input5.addEventListener("change", (e) => {
+
+let askAgain = true;
+const input5 = document.getElementById("tipoExperiencia");
+input5.addEventListener("change", (e) => {
+  if(checkearEdad()){
     let experiencia = e.target.value;
-    console.log(experiencia);
+      console.log(experiencia);
 
-    do {
-      let experiencias = document.getElementById("experienciaElegida");
-      console.log(experiencias);
+      do {
+        let experiencias = document.getElementById("experienciaElegida");
+        console.log(experiencias);
 
-      switch (experiencia.toLowerCase()) {
-        case "bailable":
-        case "pub":
-          askAgain = false;
+        switch (experiencia.toLowerCase()) {
+          case "bailable":
+          case "pub":
+            askAgain = false;
 
-          experiencias.innerHTML =
-            "Los mejores lugares para vivir la experiencia tipo " +
-            experiencia.toLowerCase() +
-            " son:" +
-            "" +
-            busquedaNombres(experiencia.toLowerCase());
+            experiencias.innerHTML =
+              "Los mejores lugares para vivir la experiencia tipo " +
+              experiencia.toLowerCase() +
+              " son: " +
+              "" +
+              busquedaNombres(experiencia.toLowerCase());
+            break;
 
-          break;
-
-        default:
-          alert("Volvé a ingresar una opción válida");
-          break;
-      }
-    } while (askAgain);
-  });
-};
-
-devolverNombre();
-comprobarEdad();
+          default:
+            alert("Volvé a ingresar una opción válida");
+            break;
+        }
+      } while (askAgain);
+    }else{
+      alert("No tienes edad suficiente")
+    }
+});
 
 document
   .getElementById("gastoParking")
   .addEventListener("change", cuantoGastar);
-
-tipoDeExperiencia();
